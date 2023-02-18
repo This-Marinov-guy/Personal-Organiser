a09df32dee07b90c6b2b54d526ba0c2ae649258d
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-02-18 20:43:22 +0000

    modified:   Server/middleware/file-upload.js

--- Server/middleware/file-upload.js
+++ Server/middleware/file-upload.js
@@ -1,6 +1,5 @@
 import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
-import { fileURLToPath } from 'url';
 
 const MIME_TYPE_MAP = {
   "image/png": "png",
@@ -8,12 +7,10 @@ const MIME_TYPE_MAP = {
   "image/jpg": "jpg",
 };
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, __dirname + "/uploads/images");
+    cb(null,"/uploads/images");
   },
   filename: (req, file, cb) => {
     const ext = MIME_TYPE_MAP[file.mimetype];
